4ac041ee3b565e98d825a7367b9b22d30f16c64e
montassar1998 <montassarriahi@aol.com>
2022-04-07 12:19:06 +0200

    Added ManyToMany SB Relationships

--- src/main/java/com/location/entities/Promotion.java
+++ src/main/java/com/location/entities/Promotion.java
@@ -1,12 +1,14 @@
 package com.location.entities;
 
 import java.sql.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Promotion {
@@ -23,4 +25,6 @@ public class Promotion {
 	@Column(name = "pourcentage")
 	private float pourcentage;
 
+	@ManyToMany(mappedBy = "voitures")
+	private List<Voiture> voitures;
 }
--- src/main/java/com/location/entities/Voiture.java
+++ src/main/java/com/location/entities/Voiture.java
@@ -4,12 +4,15 @@ import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -42,6 +45,10 @@ public class Voiture {
 
 	@OneToMany(mappedBy = "reservation")
 	private List<Reservation> reservations;
+
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "promotion", joinColumns = @JoinColumn(name = "column_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "voiture_id", referencedColumnName = "id"))
+	private List<Promotion> promotions;
 
 	public int getId() {
 		return id;
